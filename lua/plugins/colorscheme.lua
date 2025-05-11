c3492b45a3f2f659fe0620bb16e0a22abaa45c88
Evan Hoefling <evanh1393@gmail.com>
2025-05-10 23:02:50 -0400

    adding snacks

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    config = function()
+      vim.cmd([[colorscheme cyberdream]])
+
+      -- Remove all background colors
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+      vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+      vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+      vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+      vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+    end,
+  },
+}
