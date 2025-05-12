03b7a29da4afbd75c1583e8f49ddd721c5c9d515
Evan Hoefling <evanh1393@gmail.com>
2025-05-12 11:52:23 -0400

    getting-to-work-with-php

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -1,12 +1,28 @@
 return {
-  "mattn/emmet-vim",
-  ft = { "html", "blade", "vue", "javascriptreact", "typescriptreact" },
-  config = function()
-    vim.g.user_emmet_leader_key = "<C-Z>"
-    vim.g.user_emmet_mode = "a"
-    vim.g.user_emmet_install_global = 0
-    vim.cmd([[
-      autocmd FileType html,blade,vue,javascriptreact,typescriptreact EmmetInstall
-    ]])
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- @type lspconfig.options
+      servers = {
+        emmet_ls = {
+          filetypes = {
+            "astro",
+            "blade",
+            "css",
+            "eruby",
+            "html",
+            "htmldjango",
+            "javascriptreact",
+            "less",
+            "pug",
+            "sass",
+            "scss",
+            "svelte",
+            "typescriptreact",
+            "vue",
+          },
+        },
+      },
+    },
+  },
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,14 +1,19 @@
 return {
-  -- Add your LSP configuration here
   {
     "neovim/nvim-lspconfig",
     opts = {
+      -- @type lspconfig.options
       servers = {
         intelephense = {
+          filetypes = { "php", "blade", "php_only" },
           settings = {
             intelephense = {
-              -- Use the environment variable or local file
               licenceKey = vim.env.INTELEPHENSE_LICENSE,
+              filetypes = { "php", "blade", "php_only" },
+              files = {
+                associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+                maxSize = 5000000,
+              },
             },
           },
         },
